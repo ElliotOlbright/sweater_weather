b8198bb294c5a7c53d45e08fc93d330c842e4ba6
Elliot Olbright <elliotolbright@gmail.com>
2021-09-25 13:56:56 -0600

    weather service call test

---
+++ app/serializers/weather_serializer.rb
@@ -0,0 +1,4 @@
+class WeatherSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes #:current_temp, :min_temp, :max_temp, :current_conditions
+end
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -7,12 +7,27 @@ RSpec.describe WeatherService do
   end
 
   it 'can get weather for a given destination', :vcr do
-    json = WeatherService.get_destination_weather('37.5858662', '-85.67330523')
+    json = WeatherService.get_weather('37.5858662', '-85.67330523')
+    
+    expect(json).to have_key(:current)
+    expect(json[:current]).to be_a Hash
 
-    expect(json).to have_key(:weather)
-    expect(json[:weather][0]).to have_key(:description)
-    expect(json[:main]).to have_key(:temp)
-    expect(json[:main]).to have_key(:temp_min)
-    expect(json[:main]).to have_key(:temp_max)
+    expect(json).to have_key(:lat)
+    expect(json[:lat]).to be_a Float
+
+    expect(json).to have_key(:lon)
+    expect(json[:lon]).to be_a Float
+
+    expect(json).to have_key(:timezone)
+    expect(json[:timezone]).to be_a String
+
+    expect(json[:current]).to have_key(:sunrise)
+    expect(json[:current][:sunrise]).to be_an Integer
+
+    expect(json[:current]).to have_key(:sunset)
+    expect(json[:current][:sunset]).to be_an Integer
+
+    expect(json[:current]).to have_key(:temp)
+    expect(json[:current][:temp]).to be_a Float
   end
 end
