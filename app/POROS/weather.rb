a3503c05679cf4cabcde22a1a786703c7940055a
Elliot Olbright <elliotolbright@gmail.com>
2021-09-26 11:35:58 -0600

    create weather poro

--- app/POROS/weather.rb
+++ app/POROS/weather.rb
@@ -1,5 +1,29 @@
 class Weather 
+  attr_reader :lat,
+              :lon,
+              :timezone,
+              :sunrise,
+              :sunset,
+              :temp,
+              :feels_like,
+              :humidity,
+              :uvi,
+              :visibility,
+              :conditions,
+              :icon
 
-  def intitialize(data)
+  def initialize(data)
+    @lat                  = data[:lat]
+    @lon                  = data[:lon]
+    @timezone             = data[:timezone]
+    @sunrise              = data[:current][:sunrise]
+    @sunset               = data[:current][:sunset]
+    @temp                 = data[:current][:temp]
+    @feels_like           = data[:current][:feels_like]
+    @humidity             = data[:current][:humidity]
+    @uvi                  = data[:current][:uvi]
+    @visibility           = data[:current][:visibility]
+    @conditions           = data[:current][:weather][0][:description]
+    @icon                 = data[:current][:weather][0][:icon]
   end
 end 
--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -2,7 +2,7 @@ class WeatherFacade
   class << self
     def get_weather(lat, lon)
       json = WeatherService.get_weather(lat, lon)
-      Weathewr.new(json)
+      Weather.new(json)
     end 
   end 
 end
