8c5432fcc0779676ef6dd83f5bdb0bf0ac0d84a5
Elliot Olbright <elliotolbright@gmail.com>
2021-09-27 09:56:36 -0600

    create poro and facade for open library

--- app/facades/open_library_facade.rb
+++ app/facades/open_library_facade.rb
@@ -1,7 +1,10 @@
 class OpenLibraryFacade
   class << self
-    def get_books(location)
-      data = OpenLibraryService.get_books(location)
+    def get_books(location, limit)
+      data = OpenLibraryService.get_books(location, limit)
+      data[:docs].map do |book|
+        OpenLibrary.new(book)
+      end 
     end 
   end 
 end 
--- spec/facades/open_library_facade_spec.rb
+++ spec/facades/open_library_facade_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OpenLibraryFacade do
   it 'can create a books instance', :vcr do 
-    books = OpenLibraryFacade.get_books("Denver, co")
-    expect(books).to be_an_instance_of(OpenLibrary)
+    books = OpenLibraryFacade.get_books("Denver, co", 2)
+    books.each do |book|
+      expect(book).to be_an_instance_of(OpenLibrary)
+    end 
   end 
 end 
