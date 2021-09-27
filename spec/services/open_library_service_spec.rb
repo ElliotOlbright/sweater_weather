ef6dc3f77fc7a00fd10fb150261ef79c199732a0
Elliot Olbright <elliotolbright@gmail.com>
2021-09-27 09:29:10 -0600

    test for api call json attributes" do"

--- spec/services/open_library_service_spec.rb
+++ spec/services/open_library_service_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe OpenLibraryService do
     connection = OpenLibraryService.conn('http://openlibrary.org/search.json')
     expect(connection).to be_an_instance_of(Faraday::Connection)
   end 
+
+  it 'can search for books based on key words', :vcr do
+    json = OpenLibraryService.get_books('denver,co')
+
+
+    expect(json).to have_key(:docs)
+    expect(json[:docs]).to be_an Array
+
+    expect(json[:docs][0]).to have_key(:title)
+    expect(json[:docs][0][:title]).to be_a String
+
+    expect(json[:docs][0]).to have_key(:isbn)
+    expect(json[:docs][0][:isbn]).to be_an Array
+
+    expect(json[:docs][0]).to have_key(:publisher)
+    expect(json[:docs][0][:publisher][0]).to be_a String
+  end 
 end 
