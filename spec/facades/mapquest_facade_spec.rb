baa43d81e0490d4eb24822cb99010b24b43412a5
Elliot Olbright <elliotolbright@gmail.com>
2021-09-29 07:19:21 -0600

    build test for mapquest facade

--- spec/facades/mapquest_facade_spec.rb
+++ spec/facades/mapquest_facade_spec.rb
@@ -1,6 +1,16 @@
-require 'rails_helper' 
+require 'rails_helper'
 
-RSpec.describe MapquestFacade do 
-  xit 'can create a mapquest instance', :vcr do
-  end 
+describe MapquestFacade, :vcr do
+	describe "Class Methods" do
+		describe '::travel_time' do
+			it 'should return travel times', :vcr do
+				travel_time = MapquestFacade.travel_time("Denver,co", "Boulder,co")
+
+				expect(travel_time).to be_a(Hash)
+				expect(travel_time.keys).to eq([:formatted_time, :unix_time])
+				expect(travel_time[:formatted_time]).to be_a(String)
+				expect(travel_time[:unix_time]).to be_a(Numeric)
+			end
+		end
+	end
 end
